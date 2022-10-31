b86838741d567420b5875e0901bef7dfa6a455a2
b-kuch <58431879+b-kuch@users.noreply.github.com>
2022-10-31 23:31:28 +0100

    add previous audio player

---
+++ src/components/AudioPlayer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <button id="start-stream-button">dupa</button>
+    </div>
+</template>
+
+  
+<script lang="ts">
+import { Howl, Howler } from 'howler';
+const player = document.getElementById("audio-player");
+const button = document.getElementById("start-stream-button") as HTMLButtonElement;
+var sound: Howl;
+
+let request = new XMLHttpRequest();
+
+request.open("GET", "http://localhost:8000/audio/Here Comes A Big Black Cloud!! - Graverobbin.mp3", true);
+request.responseType = "blob";
+request.onload = () => {
+    console.log(request)
+    var howlSource = URL.createObjectURL(request.response)
+    sound = new Howl({
+        src: ["http://localhost:8000/audio/Here Comes A Big Black Cloud!! - Graverobbin.mp3"],
+        html5: true
+    });
+
+};
+
+request.send();
+
+button.onclick = () => {
+    sound.play();
+    
+}
+
+
+export default {
+
+// mounted() {
+
+
+
+
+
+// }
+}
+</script>
