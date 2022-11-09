f31b49936bcae48243539723d46b766e47f153b2
b-kuch <58431879+b-kuch@users.noreply.github.com>
2022-11-09 22:52:54 +0100

    add pausing

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -1,33 +1,30 @@
 <template>
     <div>
-        <button id="play-pause-song" v-on:click="playSong">play</button>
+        <button id="shuffle-songs">shuffle</button>
+        <button id="prev-song">prev</button>
+        <button id="play-pause-song" v-on:click="playSong">{{ playButtonLabel }}</button>
         <button id="next-song">next</button>
+        <button id="loop-song">loop</button>
     </div>
 </template>
 
   
 <script setup lang="ts">
+import { ref } from 'vue';
 import { Howl } from 'howler';
-// const player = document.getElementById("audio-player");
-// const button = document.getElementById("start-stream-button") as HTMLButtonElement;
 
+var sound: Howl;
+var isPlaying = false;
+var playButtonLabel = ref('Play');
+
+sound = new Howl({
+    src: ["http://localhost:8000/audio/Here Comes A Big Black Cloud!! - Graverobbin.mp3"],
+    html5: true
+});
 function playSong() {
-    var sound: Howl;
-    let request = new XMLHttpRequest();
-
-    request.open("GET", "http://localhost:8000/audio/Here Comes A Big Black Cloud!! - Graverobbin.mp3", true);
-    request.responseType = "blob";
-    request.onload = () => {
-        console.log(request)
-        var howlSource = URL.createObjectURL(request.response)
-        sound = new Howl({
-            src: ["http://localhost:8000/audio/Here Comes A Big Black Cloud!! - Graverobbin.mp3"],
-            html5: true
-        });
-        sound.play()
-    };
-
-    request.send();
+    isPlaying = !isPlaying;
+    isPlaying ? sound.play() : sound.pause();
+    playButtonLabel.value = isPlaying ? "Pause" : "Play";
 }
 
 
