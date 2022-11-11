8d72e87459f49cd4f15e46e448c356d25cf9178e
b-kuch <58431879+b-kuch@users.noreply.github.com>
2022-11-11 23:13:17 +0100

    add next song button

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -2,8 +2,8 @@
     <div>
         <button id="shuffle-songs">shuffle</button>
         <button id="prev-song">prev</button>
-        <button id="play-pause-song" v-on:click="playSong">{{ playButtonLabel }}</button>
-        <button id="next-song">next</button>
+        <button id="play-pause-song" v-on:click="playButtonAction">{{ playButtonLabel }}</button>
+        <button id="next-song" v-on:click="nextSong">next</button>
         <button id="loop-song">loop</button>
     </div>
 </template>
@@ -17,15 +17,34 @@ var sound: Howl;
 var isPlaying = false;
 var playButtonLabel = ref('Play');
 
-sound = new Howl({
-    src: ["http://localhost/stream/Here Comes A Big Black Cloud!! - Graverobbin.mp3"],
+var songs = [
+    "http://localhost/stream/Here Comes A Big Black Cloud!! - Graverobbin.mp3",
+    "http://localhost/stream/Jazz at Mladost Club - Arana.mp3"
+]
+var sounds = songs.map((song) => new Howl({
+    src: song,
     html5: true
-});
-function playSong() {
+}));
+var index = 0;
+
+function playButtonAction() {
     isPlaying = !isPlaying;
-    isPlaying ? sound.play() : sound.pause();
     playButtonLabel.value = isPlaying ? "Pause" : "Play";
+    playSong();
+}
+function playSong() {
+    isPlaying ? sounds[index].play() : sounds[index].pause();
 }
 
+function nextSong() {
+    sounds[index].pause();
+    sounds[index].seek(0);
+    isPlaying = false;
+    index++;
+    if (index >= songs.length || index <= 0) {
+        index = 0;
+    }
+    playButtonAction();
+}
 
 </script>
